9f6121051d69fca9abeafdd90452794d251b75fd
Heetae Kim <kimheetae@gmail.com>
2015-03-27 02:18:18 +0900

    track brush

--- chile.js
+++ chile.js
@@ -1,29 +1,77 @@
 /**
  * Created by heetae on 3/25/15.
  */
-var width = 960;
-var height = 650;
+var svg_width = 960;
+var svg_height = 650;
 var valueScaling = 7;
 
 var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    .attr("width", svg_width)
+    .attr("height", svg_height);
+
+var margin1 = {top: 10, right: 400, bottom: 10, left: 10},  // <---- focus
+    margin2 = {top: 10, right: 10, bottom: 200, left: 400}, // <---context
+    width1 = 960 - margin1.left - margin1.right,
+    width2 = 960 - margin2.left - margin2.right,
+    height1 = 500 - margin1.top - margin1.bottom, // <---- focus
+    height2 = 500 - margin2.top - margin2.bottom; // <------ context
+
+var x = d3.time.scale().range([0, width1]),
+    x2 = d3.scale.linear().range([0, svg_width]),
+    y = d3.scale.linear().range([height1, 0]),
+    y2 = d3.scale.linear().range([svg_height, 0]);
 //
-//var margin1 = {top: 10, right: 400, bottom: 10, left: 10},  // <---- focus
-//    margin2 = {top: 10, right: 10, bottom: 200, left: 400}, // <---context
-//    width1 = 960 - margin1.left - margin1.right,
-//    width2 = 960 - margin2.left - margin2.right,
-//    height1 = 500 - margin1.top - margin1.bottom, // <---- focus
-//    height2 = 500 - margin2.top - margin2.bottom; // <------ context
-//
-//var x = d3.time.scale().range([0, width1]),
-//    x2 = d3.time.scale().range([0, width2]),
-//    y = d3.scale.linear().range([height1, 0]),
-//    y2 = d3.scale.linear().range([height2, 0]);
-//
-//var brush = d3.svg.brush()
+
+function brushed() {
+
+    if (brush.empty()) {
+        //either the height OR the width is empty
+        x.domain( x2.domain() ); //reset X scale
+        y.domain( y2.domain() ); //reset Y scale
+    }
+
+    var extent = brush.extent();
+    x.domain( [extent[0][0] , extent[1][0] ] ); //min and max data X values
+    y.domain( [extent[0][1] , extent[1][1] ] ); //min and max data Y values
+
+    var extentRect = d3.select("g.x.brush rect.extent");
+
+    var rangeWidth  = extentRect.attr("width");
+    var rangeHeight = extentRect.attr("height");
+    var rangeLeft = extentRect.attr("x");
+    var rangeTop = extentRect.attr("y");
+
+
+
+    var rangeExtent = [
+        [x2(extent[0][0]), y2(extent[0][1])],
+        [x2(extent[1][0]), y2(extent[1][1])]
+    ];
+    var rangeWidth  = rangeExtent[1][0] - rangeExtent[0][0];
+    var rangeHeight = rangeExtent[1][1] - rangeExtent[0][1];
+
+
+//    focus.select(".area").attr("d", area);
+//    focus.select(".x.axis").call(xAxis);
+//    focus.select(".y.axis").call(yAxis);
+}
+
+var brush = d3.svg.brush()
 //    .x(x2)
-//    .on("brush", brushed);
+//    .y(y2)
+    .x(d3.scale.identity().domain([0, svg_width]))
+    .y(d3.scale.identity().domain([0, svg_height]))
+    .on("brush",  brushed)
+    .on("brush",function(){var extent = brush.extent()
+        console.log(extent[1][0],extent[1][1]);});
+
+var xScale = d3.scale.linear()
+    .domain([0,svg_width]).range([0,svg_width]);
+var yScale = d3.scale.linear()
+    .domain([0,svg_height]).range([0, svg_height]);
+
+var context = svg.append("g")
+    .attr("class", "context");
 
 //var focus = svg.append("g") << reference
 //    .attr("class", "focus")
@@ -33,11 +81,6 @@ var svg = d3.select("body").append("svg")
 //    .attr("height", 300);
 
 d3.json("graph.json", function(error, graph) {
-//    var dataSet = [ ];	// 데이터를 저장할 배열을 준비
-//    for(var i=0; i<data.length; i++){	// 데이터 줄 수만큼 반복
-//        dataSet.push(data[i].sales[0]);	// sales의 최초 데이터만 추출
-//    }
-
     graph.nodes.forEach(function(d) { // nodes coordinate scaling
         d.x = d.x/valueScaling;
         d.y = d.y/valueScaling;
@@ -47,18 +90,19 @@ d3.json("graph.json", function(error, graph) {
         d.target = graph.nodes[d.target];
     });
 
+    context.append("g")
+        .attr("class","xy brush")
+        .call(brush)
+        .selectAll("rect")
+        .attr("y",0)
+        .attr("height",svg_height);
+
 
     linksElements = svg.append("g")
         .selectAll("line")
         .data(graph.links)
         .enter()
         .append("line")	// 데이터의 개수만큼 circle 요소가 추가됨
-        //.call(function(elements){elements.each(function replaceByValue(d,i){ //JSON data의 value scale 조
-        //    var temp = d.source["x"];
-        //    d.source["x"] = temp/valueScaling;
-        //    var temp = d.source["y"];
-        //    d.source["y"] = temp/valueScaling;
-        //})})
         .attr("x1", function(d) { return d.source.x; })
         .attr("y1", function(d) { return d.source.y; })
         .attr("x2", function(d) { return d.target.x; })
@@ -81,7 +125,7 @@ d3.json("graph.json", function(error, graph) {
         .attr("cx", function (d) {
             return d["x"];
         })
-        //.call(function(elements){elements.each(function(d,i){console.log(d)})})
+//        .call(function(elements){elements.each(function(d,i){console.log(d)})})
         //.call(function(elements){elements.each(function(graph){console.log(graph)})})
         .attr("cy", function (graph) {
             return graph["y"];
