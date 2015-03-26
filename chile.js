d36fa784a86305d56d308c1ff262d0baa2ba1a79
Heetae Kim <kimheetae@gmail.com>
2015-03-26 17:44:58 +0900

    Insert edges

--- chile.js
+++ chile.js
@@ -3,6 +3,7 @@
  */
 var width = 960;
 var height = 650;
+var valueScaling = 5;
 
 var yScale = d3.scale.linear()  // 스케일 설정
     .domain([0, 8000])   // 원래 크기
@@ -18,32 +19,53 @@ d3.json("graph.json", function(error, graph) {
 //        dataSet.push(data[i].sales[0]);	// sales의 최초 데이터만 추출
 //    }
 
+    graph.links.forEach(function(d) {
+        console.log(d.source);
+        d.source = graph.nodes[d.source];
+        d.target = graph.nodes[d.target];
+    });
 
-    var circleElements = d3.select("#myGraph").append("g")
+    linksElements = d3.select("#myGraph").append("g")
+        .selectAll("line")
+        .data(graph.links)
+        .enter()
+        .append("line")	// 데이터의 개수만큼 circle 요소가 추가됨
+        //.call(function(elements){elements.each(function replaceByValue(d,i){ //JSON data의 value scale 조
+        //    var temp = d.source["x"];
+        //    d.source["x"] = temp/valueScaling;
+        //    var temp = d.source["y"];
+        //    d.source["y"] = temp/valueScaling;
+        //})})
+        .attr("x1", function(d) { return d.source.x/5; })
+        .attr("y1", function(d) { return d.source.y/5; })
+        .attr("x2", function(d) { return d.target.x/5; })
+        .attr("y2", function(d) { return d.target.y/5; })
+
+     circleElements = d3.select("#myGraph").append("g")
         .selectAll("circle")
         .data(graph.nodes)
         .enter()
         .append("circle")	// 데이터의 개수만큼 circle 요소가 추가됨
-        .call(function(elements){elements.each(function replaceByValue(d,i){ //JSON data의 value scale 조절
+        .call(function(elements){elements.each(function replaceByValue(d,i){ //JSON data의 value scale 조
             var temp = d["x"];
-            d["x"] = temp/5;
+            d["x"] = temp/valueScaling;
             var temp = d["y"];
-            d["y"] = temp/5;
+            d["y"] = temp/valueScaling;
         })})
-        .call(function(elements){elements.each(function(d,i){console.log(xScale(d["x"]))})})
+        //.call(function(elements){elements.each(function(d,i){console.log(xScale(d["x"]))})})
         .attr("class", "basic_nodes")	// CSS 클래스 지정
         .attr("cx", function (d) {
-            return d["x"];	// 최초 요소를 X 좌표로 함
+            return d["x"];
         })
         //.call(function(elements){elements.each(function(d,i){console.log(xScale(d["x"]))})})
         //.call(function(elements){elements.each(function(d,i){console.log(d)})})
+        //.call(function(elements){elements.each(function(graph){console.log(graph)})})
         .attr("cy", function (graph) {
-            return graph["y"];	// 2번째의 요소를 Y 좌표로 함
+            return graph["y"];
         })
         .attr("r", 4)	// 반지름을 지정
         .on("click",function(){d3.select(this).style("stroke", "black")})
-
-
+    
     var zoomer = d3.behavior.zoom().
         scaleExtent([0.1,10]).
         x(xScale).
