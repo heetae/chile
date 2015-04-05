425a7442345f21a01f0c5c3ff5662cc1ff8b4c13
Heetae Kim <kimheetae@gmail.com>
2015-04-05 11:56:19 +0900

    debug JSON structure changing graph

--- chile.js
+++ chile.js
@@ -250,16 +250,16 @@ d3.json("graph.json", function(error, graph) {
         var svgWidth = 320;	// SVG 요소의 넓이
         var svgHeight = 240;	// SVG 요소의 높이
         var dataSet = [10, 47, 65, 8, 64, 99, 75, 22, 63, 80];	// 데이터셋
-        var margin = svgWidth / (dataSet.length - 1);	// 꺾은선 그래프의 간격 계산
+//        var margin = svgWidth / (dataSet.length - 1);	// 꺾은선 그래프의 간격 계산
         var windowoffsetx = 600
+        var windowoffsety = 100
 
 
 //        var margin = svgWidth/(plotpoint - 1);	// 꺾은선 그래프의 간격 계산
-        var plotpoint = plotdata[20].length;
 
         nodeElements.on("click", function(){
-            pickupdata(dataSet,d3.select(this).node().__data__.id),
-            drawGraph(dataSet,d3.select(this).node().__data__.id)
+            pickupdata(dataSet,d3.select(this).node().__data__.id)
+//            drawGraph(dataSet,d3.select(this).node().__data__.id)
 
         });
 //        console.log(plotdata[20])
@@ -272,28 +272,32 @@ d3.json("graph.json", function(error, graph) {
             plotpoint=plotdata[id].length;
 //        console.log(plotdata[20][2])
             for (var i=0; i<plotpoint; i++) {	// 최초의 데이터만 처리
-                dataSet.push(plotdata[id][i]["x"]);	// 가로 한 줄 모두를 한꺼번에 넣음
+                dataSet.push([plotdata[id][i]["x"],plotdata[id][i]["y"]]);	// 가로 한 줄 모두를 한꺼번에 넣음
             }
                     console.log(dataSet);
+            drawGraph(dataSet,id);
         };
 
 // 꺾은선 그래프의 좌표를 계산하는 메서드
 
         function drawGraph(dataSet,nodeid) {
             console.log("graph draw")
-            console.log(dataSet)
+            console.log(dataSet[0])
+            var margin = svgWidth / (dataSet.length - 1);
             var delta_k = d3.svg.line()	// svg의 선
                 .x(function (d, i) {
-                    return i * margin + windowoffsetx;	// X 좌표는 표시 순서×간격
+                    return d[0]*svgHeight/19.9 + windowoffsetx;	// X 좌표는 표시 순서×간격
                 })
                 .y(function (d, i) {
-                    return svgHeight - d;	// 데이터로부터 Y 좌표 빼기
+                    return svgHeight - (d[1]*svgHeight)+windowoffsety;	// 데이터로부터 Y 좌표 빼기
                 })
 
                 // 꺾은선 그래프 그리기
             var lineElements = svg.append("path")
                 .attr("class", "line")// 데이터 수만큼 path 요소가 추가됨
                 .attr("d", delta_k(dataSet))	//연속선 지정
+
+//            svg.selectAll(delta_k).remove();
         };
 
 //
